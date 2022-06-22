2d395d734165f845b30b7193fc13c0fa5b3e37f7
Chijioke Elijah <elijahchijioke.ce@gmail.com>
2022-06-22 21:18:29 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let firstName = 'Chijioke'
+let lastName ='Elijah'
+let Height = '6.3ft'
+let Country ='Nigeria'
+
+document.write(`Name: ${firstName +" "+ lastName} <br>`)
+document.write(`Height: ${Height} <br>`)
+document.write(`Country: ${Country}`)
